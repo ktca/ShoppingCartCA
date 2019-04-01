00038c0e4d54928b5eb8e93a086afa27108c10ad
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-04-01 22:39:25 +0800

    move homeController from fonts folder to controllers

--- ShoppingCartCA/Controllers/HomeController.cs
+++ ShoppingCartCA/Controllers/HomeController.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication2.Controllers
+namespace ShoppingCartCA.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            
             return View();
         }
 

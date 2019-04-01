fa293ffd9960dbbbe62a83532828177584f167d9
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-04-01 21:45:41 +0800

    add models

---
+++ ShoppingCartCA/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartCA.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ ShoppingCartCA/Models/PurchaseDetailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartCA.Models
+{
+    public class PurchaseDetailModel
+    {
+        public int PurchaseDetailID { get; set; }
+        public int PurchaseID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}

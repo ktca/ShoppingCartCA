b35cb693ed8fad591e46041e0f15716051c979b8
Kyi Thar Chan Aye <e0395893@u.nus.edu>
2019-04-02 12:20:49 +0800

    Add Login Methods

--- ShoppingCartCA/Controllers/HomeController.cs
+++ ShoppingCartCA/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShoppingCartCA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -27,5 +28,32 @@ namespace ShoppingCartCA.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserModel objUser)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //using (SqlConnection con = new SqlConnection(connectionString))
+                using (DB_Entities db = new DB_Entities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
     }
-}
--- ShoppingCartCA/Models/UserModel.cs
+++ ShoppingCartCA/Models/UserModel.cs
@@ -9,7 +9,7 @@ namespace ShoppingCartCA.Models
     public class UserModel
     {
         public int UserID { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }

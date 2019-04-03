a4f0619dfb4f4dc56d012192c23125918708f695
ayethinzarkyaw <athinzarkyaw@gmail.com>
2019-04-03 16:12:13 +0800

    view cart (without checkout)

---
+++ ShoppingCartCA/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartCA.Classes;
+using ShoppingCartCA.Models;
+
+namespace ShoppingCartCA.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult ViewCart()
+        {
+            List<UserCartModel> cartList = new List<UserCartModel>();
+            List<int> productList = (List<int>)Session["Cart"];
+            if (productList.Count() > 0)
+            {
+                Common common = new Common();
+                cartList = common.GetProductList(productList);
+            }
+            return View(cartList);
+        }
+    }
+}
--- ShoppingCartCA/Models/UserCartModel.cs
+++ ShoppingCartCA/Models/UserCartModel.cs
@@ -7,9 +7,11 @@ namespace ShoppingCartCA.Models
 {
     public class UserCartModel
     {
-        public int cartID { get; set; }
-        public int userID { get; set; }
-        public int productID { get; set; }
+        //public int cartID { get; set; }
+        //public int userID { get; set; }
+        //public int productID { get; set; }
         public int quantity { get; set; }
+
+        public ProductModel product { get; set; }
     }
 }
